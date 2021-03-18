1c46cb21d66658f88365ac2c5b104f6b860906c8
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-18 15:14:44 +0700

    check

--- 6320503114_6_1.c
+++ 6320503114_6_1.c
@@ -2,14 +2,32 @@
 #include<string.h>
 int main()
 {
-    int n,i;
+    int n,i,j=0;
     scanf("%d",&n);
-    char word[n][1000];
-    char tmp[1000],com[1000];
+    char word[n][1000],tmp[n][1000],t[1000];
     for(i=0;i<n;i++)
+    {
         scanf("%s",word[i]);
-
-
+        strcpy(tmp[i],word[i]);
+    }
+    i=1;
+    while(i<n-1)
+    {
+        if(strmp(tmp[i-1][j],word[i][j])==0)
+        {
+            j++;
+            continue;
+        }
+        else
+        {
+            if(tmp[i][j]<word[i][j])
+            {
+                strcpy(,)
+            }
+        }
+        j=0;
+        i++;
+    }
 
 
 
