9e457444ef570c980304e596ad2c8610cfea4f9e
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-18 15:00:10 +0700

    input

--- 6320503114_6_1.c
+++ 6320503114_6_1.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    char word[n][1000];
+    char tmp[1000],com[1000];
+    for(i=0;i<n;i++)
+        scanf("%s",word[i]);
 
+
+
+
+
+
+}
