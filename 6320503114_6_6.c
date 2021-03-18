474ce9a7f20c1fd250f48ff4e8a2e0d7efed6ac4
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-18 17:19:32 +0700

    2

--- 6320503114_6_6.c
+++ 6320503114_6_6.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,j=0,k=0;
     char s[250];
     scanf("%d",&n);
     scanf("%s",&s);
@@ -13,6 +13,40 @@ int main()
             pep[i][j]='.';
         }
     }
+    for(i=0;i<strlen(s);i++)
+    {
+        if(pep[i]=='U'||pep[i]=='D')
+        {
+            if(w>0&&s[i]=='U')
+           {
+                if(ar[w][h]=='.')
+                {
+                    ar[w][h]='|';
+                }
+                else if(ar[w][h]=='-')
+                {
+                    ar[w][h]='+';
+                }
+                w--;
+                if(ar[w][h]=='.')
+                {
+                    ar[w][h]='|';
+                }
+                else if(ar[w][h]=='-')
+                {
+                    ar[w][h]='+';
+                }
+            }
+
+    }
+    /*for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%c",pep[i][j]);
+        }
+        printf("\n");
+    }*/
 
 
 }
