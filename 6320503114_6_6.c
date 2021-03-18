d968642df4a28e85a3e5bc0044f0336209a418d8
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-18 17:41:45 +0700

    com

--- 6320503114_6_6.c
+++ 6320503114_6_6.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    int n,i,j,j=0,k=0;
-    char s[250];
+    int n,i,j,w=0,h=0;
+    char cha[250];
     scanf("%d",&n);
-    scanf("%s",&s);
+    scanf("%s",&cha);
     char pep[n][n];
     for(i=0;i<n;i++)
     {
@@ -13,40 +14,70 @@ int main()
             pep[i][j]='.';
         }
     }
-    for(i=0;i<strlen(s);i++)
+    for(i=0;i<strlen(cha);i++)
     {
-        if(pep[i]=='U'||pep[i]=='D')
+      if(cha[i]=='U'||cha[i]=='D')
         {
-            if(w>0&&s[i]=='U')
-           {
-                if(ar[w][h]=='.')
-                {
-                    ar[w][h]='|';
-                }
-                else if(ar[w][h]=='-')
-                {
-                    ar[w][h]='+';
-                }
+            if(w>0&&cha[i]=='U')
+            {
+                if(pep[w][h]=='.')
+                    pep[w][h]='|';
+                else if(pep[w][h]=='-')
+                    pep[w][h]='+';
                 w--;
-                if(ar[w][h]=='.')
-                {
-                    ar[w][h]='|';
-                }
-                else if(ar[w][h]=='-')
-                {
-                    ar[w][h]='+';
-                }
+                if(pep[w][h]=='.')
+                    pep[w][h]='|';
+                else if(pep[w][h]=='-')
+                    pep[w][h]='+';
             }
-
+            if(w<n-1&&cha[i]=='D')
+            {
+                if(pep[w][h]=='.')
+                    pep[w][h]='|';
+                else if(pep[w][h]=='-')
+                    pep[w][h]='+';
+                w++;
+                if(pep[w][h]=='.')
+                    pep[w][h]='|';
+                else if(pep[w][h]=='-')
+                    pep[w][h]='+';
+            }
+        }
+        else if(cha[i]=='R'||cha[i]=='L')
+        {
+            if(h<n-1&&cha[i]=='R')
+            {
+                if(pep[w][h]=='.')
+                    pep[w][h]='-';
+                else if(pep[w][h]=='|')
+                    pep[w][h]='+';
+                h++;
+                if(pep[w][h]=='.')
+                    pep[w][h]='-';
+                else if(pep[w][h]=='|')
+                    pep[w][h]='+';
+            }
+            if(h>0&&cha[i]=='L')
+            {
+                if(pep[w][h]=='.')
+                    pep[w][h]='-';
+                else if(pep[w][h]=='|')
+                    pep[w][h]='+';
+                h--;
+                if(pep[w][h]=='.')
+                    pep[w][h]='-';
+                else if(pep[w][h]=='|')
+                    pep[w][h]='+';
+            }
+        }
     }
-    /*for(i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
         for(j=0;j<n;j++)
         {
             printf("%c",pep[i][j]);
         }
         printf("\n");
-    }*/
-
+    }
 
 }
