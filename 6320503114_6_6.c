e867814469a90d8c864df58699c148c9bebefb1d
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-18 17:08:08 +0700

    1

--- 6320503114_6_6.c
+++ 6320503114_6_6.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,i,j;
+    char s[250];
     scanf("%d",&n);
-
-
-
+    scanf("%s",&s);
+    char pep[n][n];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            pep[i][j]='.';
+        }
+    }
 
 
 }
