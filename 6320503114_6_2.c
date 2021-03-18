a608b5432c95abe6b0b69a8d706712ec279fcc44
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-18 14:55:02 +0700

    finish

--- 6320503114_6_2.c
+++ 6320503114_6_2.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,number,guess;
+    int i,n,number,guess,tmp1,tmp2,yes=0,no=0;
     scanf("%d",&n);
     scanf("%d",&number);
     scanf("%d",&guess);
-
-
+    for(i=0;i<n;i++)
+    {
+        tmp1=number%10;
+        tmp2=guess%10;
+        if(tmp1==tmp2)
+            yes++;
+        else
+            no++;
+        number/=10;
+        guess/=10;
+    }
+    printf("%d %d",yes,no);
 
 
 
